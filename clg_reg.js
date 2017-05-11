97825087df74bbb05b7910e5feac7f195bb5b772
bitnahian <bitnahian@yahoo.com>
2017-05-12 04:30:12 +1000

    fixed clc rego form

--- clg_reg.js
+++ clg_reg.js
@@ -5,22 +5,17 @@ $(document).ready(function() {
 		var password = $("#password").val();
 		var cpassword = $("#cpassword").val();
 		var address1 = $("#address1").val();
-		var address2 = $("#address2").val();
+		var phone = $("#phone").val();
 		var city = $("#city").val();
 		var state = $( "#state" ).val(); 
 		var postcode = $("#postcode").val();
 		var isvalidnum = /^\d{4}$/.test(postcode);
 
-		var specializations = document.getElementsByName("specializations");
 		var sList = "";
-
-		for(var i = 0; i < specializations.length; ++i)
-		{
-			if (specializations[i].checked)
-			{
-				sList += specializations[i].val;
-			}
-		}
+		$('input[type=checkbox]').each(function () {
+    	var sThisVal = (this.checked ? $(this).val() : " ");
+    	sList += (sList=="" ? sThisVal : " " + sThisVal)
+		});
 
 		console.log(sList);
 
@@ -34,30 +29,28 @@ $(document).ready(function() {
 		else if((address1.length) > 50){
 			alert("Address 1 line is too big.");
 		}
-		else if((address2.length) > 50){
-			alert("Address 2 line is too big.");
-		}
 		else if(!isvalidnum)
 		{
 			alert("Post code is not a number. Try again.")
 		}
-		/*
+		
 		else {
-			$.post("register.php", {
+			$.post("clg_reg.php", {
 				name1: name,
 				email1: email,
 				password1: password,
 				address11: address1,
-				address21: address2,
+				phone1: phone,
 				city1: city,
 				state1: state,
-				postcode1: postcode
+				postcode1: postcode,
+				sList1: sList
 			}, function(data) {
 				if (data == 'You have Successfully Registered.....') {
 					$("form")[0].reset();
 				}
 				alert(data);
 			});
-		}*/
+		}
 	});
 });
---
+++ clg_reg.php
@@ -0,0 +1,43 @@
+<?php
+$connection = mysqli_connect("localhost", "root"); // Establishing connection with server..
+
+if(!$connection)
+{
+	die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+$db = mysqli_select_db($connection, "pawwn"); // Selecting Database.
+$name=$_POST['name1']; // Fetching Values from URL.
+$email=$_POST['email1'];
+$address1 = $_POST['address11'];
+$phone = $_POST['phone1'];
+$city = $_POST['city1'];
+$state = $_POST['state1'];
+$postcode = $_POST['postcode1'];
+$password= sha1($_POST['password1']); // Password Encryption, If you like you can also leave sha1.
+$sList = $_POST['sList1'];
+// Check if e-mail address syntax is valid or not
+$email = filter_var($email, FILTER_SANITIZE_EMAIL); // Sanitizing email(Remove unexpected symbol like <,>,?,#,!, etc.)
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+	echo "Invalid Email.......";
+}else{
+	$result = mysqli_query($connection, "SELECT * FROM clc WHERE email='$email'");
+	$data = mysqli_num_rows($result);
+	if(($data)==0){
+//$query = mysql_query("INSERT INTO registration(name, email, password, address1, address2, city, state, postcode) 
+//					  VALUES ('$name', '$email', '$password', '$address1', '$address2' , '$city' , '$state', '$postcode')"); // Insert query
+		$stmt = $connection->prepare("INSERT INTO clc(name, email, password, address1, phone, city, state, postcode, specialisation) 
+			VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssssss", $name,$email,$password,$address1,$phone,$city,$state,$postcode, $sList);
+		if($stmt->execute()){
+//if($query){
+			echo "You have Successfully Registered.....";
+		} else {
+			echo "Error....!!";
+		}
+	}else{
+		echo "This email is already registered, Please try another email...";
+	}
+}
+mysqli_close ($connection);
+?>
