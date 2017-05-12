440a3e2c393517659cd61deac68db9d09021a180
pakiKanga <alinaeem2006@gmail.com>
2017-05-12 13:39:29 +1000

    Update

--- index.php
+++ index.php
@@ -19,19 +19,75 @@ session_start();
 
 <body>
 
+
   <!--Import jQuery before materialize.js-->
   <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 
   <script type="text/javascript" src="js/materialize.min.js"></script>
 
-  <script type="text/javascript"
-  src="js/display.js"></script>
+ 
+  
+   <!---SIDENAV STARTS HERE-->   
+
+        <ul id="slide-out" class="side-nav">
+            <li><div class="userView">
+                <div class="background">
+                    <img src="images/about.jpg">
+                </div>
+                <a href="#!user"><img class="circle" src="images/default-user.png"></a>
+                <a href="#!name"><span class="white-text name">
+                    <?php 
+                    if(isset($_SESSION['name']))
+                    { 
+                      echo $_SESSION['name'];
+                    }
+                    else
+                    {
+                      echo "Register now";
+                    }
+                    ?>
+                </span></a>
+                <a href="#!email"><span class="white-text email">
+                 <?php 
+                    if(isset($_SESSION['name']))
+                    { 
+                      echo $_SESSION['dmail'];
+                    }
+                    else
+                    {
+                      echo "";
+                    }
+                    ?>
+                    </span></a>
+                </div></li>
+
+            <li><a href="about.php">About</a></li>
+            <li><a href="consult.php">Consult</a></li>
+            <li><a href="#!">Contact</a></li>
+            <li class="no-padding">
+                <ul class="collapsible collapsible-accordion">
+                    <li>
+                        <a class="collapsible-header">Register<i class="material-icons">arrow_drop_down</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="registration.html">User</a></li>
+                                <li><a href="#!">CLC</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+
+        <!---SIDENAV ENDS HERE-->
 
   <!--NAVBAR STARTS HERE-->
 
   <div class="navbar-fixed">
    <nav class="nav-extended">
      <div class="nav-wrapper">
+        <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
+
        <ul id="dropdown1" class="dropdown-content">
         <li><a href="registration.html">CLIENT REGISTRATION</a></li>
         <li><a href="clc_reg.html">CLC REGISTRATION</a></li>
@@ -39,7 +95,7 @@ session_start();
 
       <a href="index.php" class="brand-logo center">PAWWN</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="#">ABOUT</a></li></strong>
+        <li><a href="about.php">ABOUT</a></li></strong>
         <strong>
           <li><a href="#">FAQs</a></li></strong>
           <strong>
@@ -102,6 +158,17 @@ session_start();
            <h3 class ="header col s12 white-text light center">Simplify the interactions between a client and your legal centre</h3>
          </div>
        </a>
+       
+       <div id="query-div">
+            <h1 class="header center white-text">Find the right CLC for you</h1>
+           <h3 class ="header col s12 white-text light center">Click below</h3>
+           <div class="row"> <p>
+            <a class="waves-effect waves-light btn">Client</a>
+       </div>
+           
+       
+        <script type="text/javascript"
+  src="js/display.js"></script>
 
    <!--
     FOOTER STARTS HERE
---
+++ js/flowchart.js
@@ -0,0 +1,13 @@
+//<![CDATA[
+$(window).load(function(){
+$(document) .ready(function() {
+	
+$('.c1').hide();
+$("a").click(function(){
+
+    $(this).parent().children("ul").toggle();
+
+});
+  
+  });
+});//]]> 
